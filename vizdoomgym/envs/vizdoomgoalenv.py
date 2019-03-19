cf4db8887dd89245eda58bb8de4d65b7f09f5e78
Aravind Kumaraguru <5566152+akumaraguru@users.noreply.github.com>
2019-03-18 18:02:34 -0700

    Update my_way_home_goal env
    - Removed goal objects
    - Provides goal (x, y, angle) data to python
    - Added position/angle noise to goal and start location
    - Cleaned up ACS script
    - Added closeness checking in python wrapper

--- vizdoomgym/envs/vizdoomgoalenv.py
+++ vizdoomgym/envs/vizdoomgoalenv.py
@@ -1,6 +1,7 @@
 import logging
 
 from gym import spaces
+import numpy as np
 
 from vizdoomgym.envs import VizdoomEnv
 
@@ -34,7 +35,7 @@ class VizdoomGoalEnv(VizdoomEnv):
             obs, _, _, _ = super(VizdoomGoalEnv, self).step(0)
             state = self.game.get_state()
             variables = state.game_variables
-            goal_obs_generated = variables[-1]
+            goal_obs_generated = variables[-1]  # 'ready' user variable
             if goal_obs_generated >= 1:
                 break
 
@@ -42,6 +43,17 @@ class VizdoomGoalEnv(VizdoomEnv):
 
     def step(self, action):
         obs, reward, done, info = super(VizdoomGoalEnv, self).step(action)
+
+        # Check closeness with goal position
+        variables = self.game.get_state().game_variables
+        agent_x, agent_y, agent_a = variables[1], variables[2], variables[3]
+        goal_x, goal_y, goal_a = variables[4], variables[5], variables[6]
+
+        if (np.abs(agent_x - goal_x) < 10. and
+            np.abs(agent_y - goal_y) < 10. and
+            # https://gamedev.stackexchange.com/a/4472
+            180 - np.abs(np.abs(agent_a - goal_a) - 180) < 10.):
+            done = True
         return self._make_observation(obs), reward, done, info
 
 
