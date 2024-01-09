b9b3aa9bce6c222b5d4165ab5096d18b831fdbc7
ref-std <101884262+refstd@users.noreply.github.com>
2024-01-09 16:20:16 +0900

    Update vizdoomenv.py
    observation bugfix

--- vizdoomgym/envs/vizdoomenv.py
+++ vizdoomgym/envs/vizdoomenv.py
@@ -166,6 +166,7 @@ class VizdoomEnv(gym.Env):
         state = self.game.get_state()
         done = self.game.is_episode_finished()
         if not done:
+            observation = state.screen_buffer
             info.update(self.get_info())
             self._update_histogram(info)
         else:
